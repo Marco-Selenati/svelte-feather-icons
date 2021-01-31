1324920c3bec82b0f4156065b4edf412521232c1
Marco Selenati <marcoselenati@gmail.com>
2021-01-31 14:32:15 +0100

    Remove umd

--- rollup.config.js
+++ rollup.config.js
@@ -7,23 +7,12 @@ import { terser } from "rollup-plugin-terser";
 
 import pkg from "./package.json";
 
-const name = pkg.name
-  .replace(/^(@\S+\/)?(svelte-)?(\S+)/, "$3")
-  .replace(/^\w/, (m) => m.toUpperCase())
-  .replace(/-\w/g, (m) => m[1].toUpperCase());
-
 export default {
   input: "src/index.ts",
   output: [
     {
-      file: pkg.module,
-      format: "es",
-      sourcemap: true,
-    },
-    {
       file: pkg.main,
-      format: "umd",
-      name,
+      format: "es",
       sourcemap: true,
     },
   ],
