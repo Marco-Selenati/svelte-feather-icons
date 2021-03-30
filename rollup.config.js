9ae323ac7d415c007cead60b7e63473ab0b247c7
Marco Selenati <marcoselenati@gmail.com>
2021-03-30 15:32:18 +0200

    Fix Cannot find module '@rollup/plugin-commonjs'

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,7 @@
 import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
 import typescript from "rollup-plugin-typescript2";
 import svelte from "rollup-plugin-svelte";
 import autoPreprocess from "svelte-preprocess";
-import { terser } from "rollup-plugin-terser";
 
 import pkg from "./package.json";
 
